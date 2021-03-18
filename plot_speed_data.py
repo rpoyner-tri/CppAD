58f0f339e0c8c254de6b26abc67a7cd21b4e5c3f
Rick Poyner <rick.poyner@tri.global>
2021-03-18 11:33:35 -0400

    wip

--- plot_speed_data.py
+++ plot_speed_data.py
@@ -15,28 +15,42 @@ CASES = [
 ]
 
 LIBS = [
+    "cppad",
     "double",
     "drake",
-    "cppad",
 ]
 
+MARKERS = {
+    "cppad": "x",
+    "double": "+",
+    "drake": "D",
+}
 
-def plot_case(libs, case):
+
+def plot_case(axx, libs, case):
     sizes = libs["cppad"][case][0]["size"]
 
     for lib in LIBS:
-        plt.plot(sizes, libs[lib][case]["mean"])
-    plt.title(f"Speed test case: {case}")
-    plt.xlabel("Problem size")
-    plt.ylabel("Elapsed time (sec)")
-    plt.show()
+        durations = libs[lib][case]["mean"]
+        axx.plot(sizes, durations, label=lib, marker=MARKERS[lib])
+        # axx.annotate(lib, (sizes[-1], durations[-1]))
+    axx.set_title(f"Speed test case: {case}")
+    axx.set_xlabel("Problem size")
+    axx.set_ylabel("Elapsed time (sec)")
+    axx.legend()
 
 def main():
     with shelve.open("speed_data") as db:
         libs = db["speed_data"]
 
-    for case in CASES:
-        plot_case(libs, case)
-        plt.figure()
+    fig, axs = plt.subplots(3, 2)
+
+    assert len(axs.flat) >= len(CASES)
+    pairs = zip(CASES, axs.flat)
+
+    for case, axx in pairs:
+        plot_case(axx, libs, case)
+    plt.show()
+
 if __name__ == "__main__":
     main()
